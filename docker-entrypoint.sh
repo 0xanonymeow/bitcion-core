a9f9fc4c112184e5f44226336a43b442d0c071d4
0xanonymeow <anonymeow@incatni.to>
2024-01-30 17:46:21 +0700

    init

---
+++ docker-entrypoint.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+set -e
+
+if [ "$echo "$1" | cut -c1-1" = "-" ]; then
+  set -- bitcoind "$@"
+fi
+
+if [ "$1" = "bitcoind" ] || [ "$1" = "bitcoin-cli" ]; then
+    set -- "$1" -conf="$COIN_CONF_FILE" "${@:2}"
+fi
+
+if [ "$1" = "bitcoind" ]; then
+    set -- "$@" -printtoconsole
+fi
+
+echo "$@"
+if [ "$1" = "bitcoind" ] || [ "$1" = "bitcoin-cli" ] || [ "$1" = "bitcoin-tx" ]; then
+    exec gosu "$(whoami)" "$@"
+else
+    exec "$@"
+fi
---
+++ scripts/walletnotify.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+TRANSACTION_ID="$1"
+echo -e "Transaction from Wallet: ${TRANSACTION_ID}"
